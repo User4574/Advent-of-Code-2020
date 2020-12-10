6f639955352739238f52832e0413b44d620e340d
Nat.Lasseter <user@4574.co.uk>
2020-12-10 14:32:43 +0000

    Day 10. DUH.

--- day10/part2.rb
+++ day10/part2.rb
@@ -5,18 +5,10 @@ memo = {}
   memo[a] = adapters.select { |b| b > a && b <= (a + 3) }
 end
 
-device = adapters.max + 3
-stack = [0]
-count = 0
+memo[adapters.max] = 1
 
-until stack.empty? do
-  this = stack.pop
-  nxt = memo[this]
-  if nxt.empty?
-    count += 1 if (this + 3) >= device
-  else
-    stack += nxt
-  end
+(adapters.reverse[1..-1] + [0]).each do |a|
+  memo[a] = memo[a].map { |p| memo[p] }.sum
 end
 
-puts count
+puts memo[0]
